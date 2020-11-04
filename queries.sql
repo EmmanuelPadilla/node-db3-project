1e943bb1fea1d025cba192b0e65c22210503b09e
Emmanuel Padilla <manolin923@gmail.com>
2020-11-04 13:00:17 -0800

    initial commit

--- queries.sql
+++ queries.sql
@@ -1,6 +1,12 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+SELECT 
+    p.Productname,
+    c.categoryname
+ FROM product as p
+ JOIN category as c
+ ON p.Categoryid = c.id
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
