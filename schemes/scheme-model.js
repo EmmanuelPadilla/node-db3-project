3a04a6db2810d56f3053cdd98acf82246bc27a01
Emmanuel Padilla <manolin923@gmail.com>
2020-11-04 17:02:24 -0800

    fully working scheme.

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -18,19 +18,22 @@ function findById(id) {
 function findSteps(id) {
   return db("schemes as sc")
     .join("steps as st", "sc.id", "st.scheme_id")
-    .select(
-      "sc.scheme_name",
-      "st.step_number",
-      "st.instructions",
-      "sc.id"
-    )
+    .select("sc.scheme_name", "st.step_number", "st.instructions", "sc.id")
     .orderBy("st.step_number")
-    .where('sc.id', id);
+    .where("sc.id", id);
 }
 
-function add() {}
-function remove(id) {}
+async function add(scheme) {
+  const [id] = await db("schemes").insert(scheme);
+  return db("schemes").where({ id }).first();
+}
 
+async function remove(id) {
+  const scheme = await db("schemes").where({ id }).first();
+  if (!scheme) return Promise.resolve(null);
+  await db("schemes").where({ id }).del();
+  return Promise.resolve(scheme);
+}
 
 // findSteps(id) {
 //     return db('schemes as sc')
