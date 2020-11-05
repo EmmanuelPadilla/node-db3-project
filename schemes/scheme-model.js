87c853751e06a9cce3200d65f732b4fd1afca91f
Emmanuel Padilla <manolin923@gmail.com>
2020-11-04 16:48:06 -0800

    partially working scheme. needs add and remove

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,41 @@
-// scheme-model
+// // scheme-model
+const db = require("../data/db-config");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  remove,
+};
+
+function find() {
+  return db("schemes");
+}
+function findById(id) {
+  return db("schemes").where({ id }).first();
+}
+function findSteps(id) {
+  return db("schemes as sc")
+    .join("steps as st", "sc.id", "st.scheme_id")
+    .select(
+      "sc.scheme_name",
+      "st.step_number",
+      "st.instructions",
+      "sc.id"
+    )
+    .orderBy("st.step_number")
+    .where('sc.id', id);
+}
+
+function add() {}
+function remove(id) {}
+
+
+// findSteps(id) {
+//     return db('schemes as sc')
+//       .join('steps as st', 'sc.id', 'st.scheme_id')
+//       .select('sc.id', 'instructions', 'scheme_name', 'step_number')
+//       .orderBy('st.step_number')
+//       .where('sc.id', id);
+//   }
